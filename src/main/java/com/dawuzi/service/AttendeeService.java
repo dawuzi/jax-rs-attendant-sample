d4a0f79f7918cd801ba5ca3aab36e9dc54dcb992
okafor ezewuzie <oka4wuzi@gmail.com>
2017-11-18 10:27:58 +0100

    Enabled order by all valid fields in the attendee object

--- src/main/java/com/dawuzi/service/AttendeeService.java
+++ src/main/java/com/dawuzi/service/AttendeeService.java
@@ -1,11 +1,20 @@
 package com.dawuzi.service;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
@@ -97,6 +106,8 @@ public class AttendeeService {
 
 	public List<Attendee> getSomeAttendees(int start, int size, String order) {
 		
+		validateOrder(order); 
+		
 		List<Attendee> all = mockDatabase.findAll();
 		
 		if(all == null || all.isEmpty()){
@@ -108,17 +119,102 @@ public class AttendeeService {
 			return Collections.emptyList();
 		}
 		
-		if(order != null && order.isEmpty()){
-			
-			if(order.equals("id")){
-				all.sort( (a1, a2) -> {return (int)(a1.getId() - a2.getId());});
-			} else if (order.equals("email")){
-				all.sort( (a1, a2) -> {return (a1.getEmail().compareTo(a2.getEmail()));});
-			}
-		}
+		sortAttendees(all, order);
 		
 		int endIndex = Math.min(start+size, all.size());
 		
 		return all.subList(start, endIndex);
 	}
+	
+	private void sortAttendees(List<Attendee> all, String order) {
+		
+		if(order == null || order.isEmpty()){
+			return;
+		}
+		all.sort((a1, a2) -> {
+			
+			if(a1 == null || a2 == null){
+				return nullCompare(a1, a2);
+			}
+			
+			Field attendeeField = getAttendeeField(order);
+			
+			Object val1 = getFieldValue(attendeeField, a1);
+			Object val2 = getFieldValue(attendeeField, a2);
+			
+			if(val1 == null || val2 == null){
+				return nullCompare(val1, val2); 
+			}
+			
+			@SuppressWarnings({ "rawtypes", "unchecked" })
+			int compareTo = ((Comparable)val1).compareTo(val2);
+			
+			return compareTo;
+		});
+	}
+
+	private int nullCompare(Object val1, Object val2) {
+		if(val1 == null && val2 == null){
+			return 0;
+		}
+		if(val1 == null){
+			return -1;
+		}
+		if(val2 == null){
+			return 1;
+		}
+		throw new IllegalArgumentException("at least one must be null");
+	}
+
+	private Object getFieldValue(Field field, Object obj) {
+		
+		try {
+			return new PropertyDescriptor(field.getName(), field.getDeclaringClass())
+					.getReadMethod()
+					.invoke(obj);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | IntrospectionException e) {
+			throw new WebApplicationException(e, Status.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	private Field getAttendeeField(String field) {
+		try {
+			return Attendee.class.getDeclaredField(field);
+		} catch (NoSuchFieldException | SecurityException e) {
+			throw new WebApplicationException(e, Status.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	private void validateOrder(String order) {
+		
+		if(order != null && !order.isEmpty() && !isValidAttendeeField(order)){
+
+			String validFields = getValidAttendeeFields()
+					.stream()
+					.collect(Collectors.joining(", "));
+			
+			String errorDescription = "Invalid order specified. Valid orders are "+validFields;
+			
+			Response response = Response.status(Status.BAD_REQUEST).entity(new ResponseMessage(errorDescription)).build();
+			
+			throw new WebApplicationException(response);
+		}
+	}
+
+	private Set<String> getValidAttendeeFields() {
+		
+		Stream<Field> fields = Arrays.stream(Attendee.class.getDeclaredFields());
+		
+		Set<String> results = fields
+				.map(f -> f.getName())
+				.filter(name -> !name.equals("serialVersionUID"))
+				.collect(Collectors.toSet());
+		
+		return results;
+	}
+
+	private boolean isValidAttendeeField(String order) {
+		return getValidAttendeeFields().contains(order);
+	}
+
 }
